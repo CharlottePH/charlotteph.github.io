f53a3d9059a63ceba1e59e98cb7aac782f14c4a0
CharlottePH <cperryhouts@gmail.com>
2019-09-06 15:03:17 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ fetch(
         //mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
-            'Authorization': 'Bearer QnNpWEtRSFNkN2Zqa1BZUlpZQVN5dz09',
-            'Origin': 'https://charlotteph.github.io/'
+            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpcCI6WzQ4LDQ2LDQ4LDQ2LDQ4LDQ2LDQ4XSwiaXNzdWVyX2lkIjoxMjIwLCJvcmlnaW4iOiJodHRwczovL2NoYXJsb3R0ZXBoLmdpdGh1Yi5pbyIsImF1ZCI6Ikpva2VuIiwiZXhwIjoxNTY3Nzg1NzQ0LCJpYXQiOjE1Njc3Nzg1NDQsImlzcyI6Ikpva2VuIiwianRpIjoiMm4wdHNoczNycW1pZ21qczVnYzdhNjMxIiwibmJmIjoxNTY3Nzc4NTQ0fQ.2tB5ORA_xrQleiS85gEgCfYz-ZDKWXfnvd2ZVvGP0IQ',
+            'Origin': 'https://charlotteph.github.io'
         }
     })
   .then(function(response) {
