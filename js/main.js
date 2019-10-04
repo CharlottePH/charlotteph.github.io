a981664b09af11fb61f2edaac43ccad40963d572
CharlottePH <cperryhouts@gmail.com>
2019-10-04 12:23:27 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpcCI6WzQ4LDQ2LDQ4LDQ2LDQ4LDQ2LDQ4XSwiaXNzdWVyX2lkIjoxMjIwLCJvcmlnaW4iOiJodHRwczovL2NoYXJsb3R0ZXBoLmdpdGh1Yi5pbyIsImF1ZCI6Ikpva2VuIiwiZXhwIjoxNTcwMTk0NzU2LCJpYXQiOjE1NzAxODc1NTYsImlzcyI6Ikpva2VuIiwianRpIjoiMm41NnFncXVob2xnaWRwaG5zaDlmcGYxIiwibmJmIjoxNTcwMTg3NTU2fQ.Y_fFz0dAkjM6RCw5DF-66rAIZRqjsTsCBMBgfeE4wHA";
 
-const myHeaders = new Headers({
-  'Authorization': 'Bearer ' + token
-});
+// const myHeaders = new Headers({
+//   'Authorization': 'Bearer ' + token
+// });
 
 const myInit = {
-  method: 'GET',
-  headers: myHeaders
+  method: 'GET'
 };
 
-const myRequest = new Request('https://trefle.io/api/kingdoms', myInit);
+const myRequest = new Request('https://trefle.io/api/kingdoms?token=' + token, myInit);
 
 fetch(myRequest)
   .then(function(response) {
